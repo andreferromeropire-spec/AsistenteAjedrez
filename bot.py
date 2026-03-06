0d9ed2a339bbd69527ae2e91964614d8e9c42670
Andrea <andrefer.romeropire@gmail.com>
2026-03-06 04:21:41 -0300

    comentario de prueba de deploy

--- bot.py
+++ bot.py
@@ -1265,3 +1265,5 @@ if __name__ == "__main__":
     scheduler = configurar_scheduler()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+    #comentario de prueba
